7008bc035e74f6a618336424d1d61c35ecf4929d
sw-yx <shawnthe1@gmail.com>
2022-01-21 08:11:03 +0800

    Initial commit from @temporalio/create

---
+++ src/client.ts
@@ -0,0 +1,31 @@
+import { Connection, WorkflowClient } from '@temporalio/client';
+import { example } from './workflows';
+
+async function run() {
+  const connection = new Connection({
+    // // Connect to localhost with default ConnectionOptions.
+    // // In production, pass options to the Connection constructor to configure TLS and other settings:
+    // address: 'foo.bar.tmprl.cloud', // as provisioned
+    // tls: {} // as provisioned
+  }); 
+
+  const client = new WorkflowClient(connection.service, {
+    // namespace: 'default', // change if you have a different namespace
+  });
+
+  const handle = await client.start(example, {
+    args: ['Temporal'], // type inference works! args: [name: string]
+    taskQueue: 'tutorial',
+    // in practice, use a meaningful business id, eg customerId or transactionId
+    workflowId: 'wf-id-' + Math.floor(Math.random() * 1000), 
+  });
+  console.log(`Started workflow ${handle.workflowId}`);
+
+  // optional: wait for client result
+  // console.log(await handle.result()); // Hello, Temporal!
+}
+
+run().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
